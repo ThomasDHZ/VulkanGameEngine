f811200eab2e60b9bb1200e673bf1f80c8be79ef
ThomasDHZ <dothackzero@outlook.com>
2025-12-04 23:43:28 -0500

    a

--- VulkanGameEngineAndroid/app/src/main/java/com/example/vulkangameengineandroid/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.vulkangameengineandroid;
-
-import com.google.androidgamesdk.GameActivity;
-
-public class MainActivity extends GameActivity {
-    static {
-        System.loadLibrary("VulkanGameEngine");
-    }
-}
