f4d111df1caf509bc2cdf86985b28e1f6403ee98
ThomasDHZ <dothackzero@outlook.com>
2026-02-21 01:01:03 -0500

    Working on MemoryPool

--- VulkanEngineDLL/MemoryPoolSystem.cpp
+++ VulkanEngineDLL/MemoryPoolSystem.cpp
@@ -1,3 +1,90 @@
-//#include "MemoryPoolSystem.h"
-//
-//MemoryPoolSystem& memoryPoolSystem = MemoryPoolSystem::Get();
+#include "MemoryPoolSystem.h"
+#include "MeshSystem.h"
+#include "MaterialSystem.h"
+#include "LightSystem.h"
+
+MemoryPoolSystem& memoryPoolSystem = MemoryPoolSystem::Get();
+
+void MemoryPoolSystem::ResizeMemoryPool()
+{
+    uint32 newIndex = 0;
+    UpdateMemoryPoolHeader();
+    const uint32 newBufferSize = sizeof(MemoryPoolBufferHeader2) + GPUMemoryPoolHeader.PointLightOffset + (sizeof(PointLight) * PointLightCount);
+
+    Vector<byte> newObjectDataPool(newBufferSize, 0xFF);
+    Vector<byte> newIsActive(newBufferSize, 0x00);
+    for (uint32 oldIndex = 0; oldIndex < GPUBufferMemoryPool.size(); ++oldIndex)
+    {
+        if (IsActive[oldIndex])
+        {
+            newObjectDataPool[newIndex] = std::move(GPUBufferMemoryPool[oldIndex]);
+            newIsActive[newIndex] = 1;
+            newIndex++;
+        }
+    }
+    FreeIndices.clear();
+
+    for (uint32 x = newIndex; x < newBufferSize; ++x)
+    {
+        FreeIndices.push_back(x);
+    }
+
+    uint32 newBufferId = bufferSystem.VMACreateDynamicBuffer(nullptr, newBufferSize, bufferSystem.FindVulkanBuffer(GPUBufferIndex).BufferUsage);
+    bufferSystem.VMAUpdateDynamicBuffer(newBufferId, newObjectDataPool.data(), 0u);
+    if (GPUBufferIndex != UINT32_MAX)
+    {
+        bufferSystem.DestroyBuffer(bufferSystem.FindVulkanBuffer(GPUBufferIndex));
+    }
+
+    GPUBufferIndex = newBufferId;
+    GPUBufferMemoryPool = std::move(newObjectDataPool);
+    IsActive = std::move(newIsActive);
+    ActiveCount = newIndex;
+    IsBufferDirty = true;
+}
+
+void MemoryPoolSystem::UpdateMemoryPoolHeader()
+{
+    if (IsHeaderDirty)
+    {
+        GPUMemoryPoolHeader.MeshOffset = 0,
+        GPUMemoryPoolHeader.MeshCount = MeshCount,
+            GPUMemoryPoolHeader.MeshSize = sizeof(MeshPropertiesStruct),
+            GPUMemoryPoolHeader.MaterialOffset = GPUMemoryPoolHeader.MeshOffset + GPUMemoryPoolHeader.MeshSize,
+            GPUMemoryPoolHeader.MaterialCount = MaterialCount,
+            GPUMemoryPoolHeader.MaterialSize = sizeof(GPUMaterial),
+            GPUMemoryPoolHeader.DirectionalLightOffset = GPUMemoryPoolHeader.MaterialOffset + GPUMemoryPoolHeader.MaterialSize,
+            GPUMemoryPoolHeader.DirectionalLightCount = DirectionalLightCount,
+            GPUMemoryPoolHeader.DirectionalLightSize = sizeof(DirectionalLight),
+            GPUMemoryPoolHeader.PointLightOffset = GPUMemoryPoolHeader.DirectionalLightOffset + GPUMemoryPoolHeader.DirectionalLightSize,
+            GPUMemoryPoolHeader.PointLightCount = PointLightCount,
+            GPUMemoryPoolHeader.PointLightSize = sizeof(PointLight);
+        IsHeaderDirty = false;
+    }
+}
+
+void MemoryPoolSystem::StartUp()
+{
+    MemorySubPoolHeader[kMeshBuffer] = MemoryPoolSubBufferHeader
+    {
+       .ActiveCount = UINT32_MAX,
+       .Offset = sizeof(MeshPropertiesStruct),
+       .Count = UINT32_MAX,
+       .Size = UINT32_MAX,
+       .IsActive,
+       .FreeIndices
+    }
+}
+
+const Vector<VkDescriptorBufferInfo> MemoryPoolSystem::GetMemoryPoolBufferInfo() const
+{
+    return Vector<VkDescriptorBufferInfo>
+    {
+        VkDescriptorBufferInfo
+        {
+            .buffer = bufferSystem.FindVulkanBuffer(GPUBufferIndex).Buffer,
+            .offset = 0,
+            .range = VK_WHOLE_SIZE
+        }
+    };
+}
--- VulkanEngineDLL/MemoryPoolSystem.h
+++ VulkanEngineDLL/MemoryPoolSystem.h
@@ -1,191 +1,87 @@
-//#pragma once
-//#include "Platform.h"
-//#include "ShaderSystem.h"
-//
-//struct ShaderStructMemoryPool
-//{
-//	uint32 ShaderStructId = MAXUINT32;
-//	uint32 ShaderStructData = MAXUINT32;
-//	uint32 BufferId = MAXUINT32;
-//	size_t ObjectSize = 0;
-//	uint32 ObjectCount = 0;
-//};
-//
-//template <typename T>
-//size_t GetMemberOffset(const String& name)
-//{
-//	const auto& map = ReflectedMembers<T>::GetOffsetMap();
-//	auto it = map.find(name);
-//	if (it == map.end())
-//	{
-//		LogError("Unknown member '{}' in struct {}", name, typeid(T).name());
-//		return 0;
-//	}
-//	return it->second;
-//}
-//
-//#define REFLECT_MEMBER(structName, memberName) \
-//    reflectMap[#memberName] = offsetof(structName, memberName);
-//
-//#define BEGIN_REFLECT(structName) \
-//    template <> \
-//    struct ReflectedMembers<structName> { \
-//        static const UnorderedMap<String, size_t>& GetOffsetMap() { \
-//            static UnorderedMap<String, size_t> reflectMap; \
-//            if (reflectMap.empty()) {
-//
-//#define END_REFLECT() \
-//            } \
-//            return reflectMap; \
-//        } \
-//    };
-//
-//class MemoryPoolSystem
-//{
-//public:
-//	static MemoryPoolSystem& Get();
-//
-//private:
-//	MemoryPoolSystem() = default;
-//	~MemoryPoolSystem() = default;
-//	MemoryPoolSystem(const MemoryPoolSystem&) = delete;
-//	MemoryPoolSystem& operator=(const MemoryPoolSystem&) = delete;
-//	MemoryPoolSystem(MemoryPoolSystem&&) = delete;
-//	MemoryPoolSystem& operator=(MemoryPoolSystem&&) = delete;
-//
-//	static constexpr uint32 FailedToFind = static_cast<uint32>(-1);
-//	static constexpr byte  MemoryBlockUsed = 1;
-//	static constexpr byte  FreeMemoryBlock = 0;
-//
-//	template <typename T>
-//	uint32 FindNextFreeMemoryBlockIndex(ShaderStructMemoryPool<T>& memoryPool)
-//	{
-//		auto itr = std::find(memoryPool.MemoryBlockInUse.begin(), memoryPool.MemoryBlockInUse.end(), FreeMemoryBlock);
-//		if (itr != memoryPool.MemoryBlockInUse.end())
-//		{
-//			return static_cast<uint32>(std::distance(memoryPool.MemoryBlockInUse.begin(), itr));
-//		}
-//		return FailedToFind;
-//	}
-//
-//	template <typename T>
-//	void ValidateStructLayout(const ShaderStructDLL& shaderStruct)
-//	{
-//		size_t currentOffset = 0;
-//		for (auto& variable : shaderStruct.ShaderBufferVariableList)
-//		{
-//			size_t alignment = variable.ByteAlignment;
-//			currentOffset = (currentOffset + alignment - 1) & ~(alignment - 1);
-//
-//			size_t cppOffset = GetMemberOffset<T>(variable.Name);
-//			if (cppOffset != currentOffset)
-//			{
-//				LogError("Offset mismatch in struct '{}': member '{}' -> C++ offset {}, shader expects {}",
-//					shaderStruct.Name, var.Name, cppOffset, currentOffset);
-//			}
-//
-//			if (variable.Size != GetMemberSize<T>(variable.Name))
-//			{
-//				LogError("Size mismatch for member '{}.{}': C++ {} bytes, shader {} bytes",
-//					shaderStruct.Name, variable.Name, GetMemberSize<T>(variable.Name), variable.Size);
-//			}
-//			currentOffset += var.Size;
-//		}
-//
-//		if (currentOffset != shaderStruct.ShaderBufferSize)
-//		{
-//			LogWarning("Shader struct '{}' total size computation drift ({} vs {})",
-//				shaderStruct.Name, currentOffset, shaderStruct.ShaderBufferSize);
-//		}
-//	}
-//public:
-//
-//	Vector<ShaderStructMemoryPool>          PipelineShaderStructMemoryPoolMap;
-//	Vector<Vector<byte>>					MemoryBlockPtr;
-//	Vector<Vector<byte>>					MemoryBlockInUse;
-//
-//	template <typename T>
-//	void CreateMemoryPool(const String& shaderStructName, uint32 objectCount)
-//	{
-//		PipelineShaderStructMemoryPoolMap.emplace_back(ShaderStructMemoryPool<T>
-//		{
-//			.ObjectSize = sizeof(T),
-//			.ObjectCount = objectCount,
-//			.MemoryBlockPtr = Vector<byte>(sizeof(T) * objectCount, 0x00),
-//			.MemoryBlockInUse = Vector<byte>(objectCount, 0x00)
-//		});
-//	}
-//
-//	template <typename T>
-//	void AddObject(ShaderStructMemoryPool<T>& memoryPool, T& shaderStruct)
-//	{
-//		int memoryIndex = FindNextFreeMemoryBlockIndex(memoryPool);
-//		memoryPool.MemoryBlockInUse[memoryIndex] = FreeMemoryBlock;
-//	}
-//
-//	template <typename T>
-//	void UpdateObject(ShaderStructMemoryPool& memoryPool, T& shaderStruct)
-//	{
-//		memoryPool.
-//	}
-//
-//	void DestroyObject(uint32 memoryIndex)
-//	{
-//		if (memoryIndex < ObjectCount && MemoryBlockInUse[memoryIndex])
-//		{
-//			reinterpret_cast<T*>(MemoryBlockPtr + (memoryIndex * ObjectSize))->Destroy();
-//			MemoryBlockInUse[memoryIndex] = FreeMemoryBlock;
-//		}
-//	}
-//
-//	void Destroy()
-//	{
-//		for (uint32 x = 0; x < MemoryBlockInUse.size(); x++)
-//		{
-//			if (MemoryBlockInUse[x]) {
-//				reinterpret_cast<T*>(MemoryBlockPtr + (x * ObjectSize))->Destroy();
-//			}
-//		}
-//
-//		if (MemoryBlockPtr)
-//		{
-//			delete[] reinterpret_cast<std::aligned_storage_t<sizeof(T), alignof(T)>*>(MemoryBlockPtr);
-//			MemoryBlockPtr = nullptr;
-//		}
-//
-//		MemoryBlockInUse.clear();
-//	}
-//
-//	Vector<T*> ViewMemoryPool()
-//	{
-//		Vector<T*> memoryList;
-//		for (uint32 x = 0; x < ObjectCount; x++)
-//		{
-//			T* ptr = reinterpret_cast<T*>(MemoryBlockPtr + (x * ObjectSize));
-//			memoryList.emplace_back(ptr);
-//		}
-//		return memoryList;
-//	}
-//
-//	void ViewMemoryMap()
-//	{
-//		const auto memory = ViewMemoryPool();
-//		std::cout << "Memory Map of the " << typeid(T).name() << " Memory Pool(" << sizeof(T) << " bytes each, "
-//			<< std::to_string(sizeof(T) * memory.size()) << " bytes total) : " << std::endl;
-//		std::cout << std::setw(20) << "Address" << std::setw(15) << "Value" << std::endl;
-//
-//		for (size_t x = 0; x < memory.size(); x++)
-//		{
-//			std::cout << std::hex << "0x" << reinterpret_cast<void*>(memory[x]) << ": "
-//				<< (MemoryBlockInUse[x] == MemoryBlockUsed ? memory[x]->Name : "nullptr") << std::endl;
-//		}
-//
-//		std::cout << std::endl << std::endl;
-//	}
-//};
-//extern DLL_EXPORT MemoryPoolSystem& memoryPoolSystem;
-//inline MemoryPoolSystem& MemoryPoolSystem::Get()
-//{
-//	static MemoryPoolSystem instance;
-//	return instance;
-//}
+#pragma once
+#include "Platform.h"
+#include "ShaderSystem.h"
+#include "MemoryPool.h"
+
+enum MemoryPoolTypes
+{
+	kMeshBuffer,
+	kMaterialBuffer,
+	kDirectionalLightBuffer,
+	kPointLightBuffer
+};
+
+struct MemoryPoolSubBufferHeader
+{
+	uint32					ActiveCount = UINT32_MAX;
+	uint32					Offset = UINT32_MAX;
+	uint32					Count = UINT32_MAX;
+	uint32					Size = UINT32_MAX;
+	Vector<byte>			IsActive;         // 0 = inactive, 1 = active
+	Vector<uint32>			FreeIndices;
+};
+
+struct MemoryPoolBufferHeader
+{
+	//CPU side: in bytes
+	//GPU side: by index (bytes/4)
+	//GPU data array: by index (bytes/4)
+	uint MeshOffset = UINT32_MAX;
+	uint MeshCount = UINT32_MAX;
+	uint MeshSize = UINT32_MAX;
+	uint MaterialOffset = UINT32_MAX;
+	uint MaterialCount = UINT32_MAX;
+	uint MaterialSize = UINT32_MAX;
+	uint DirectionalLightOffset = UINT32_MAX;
+	uint DirectionalLightCount = UINT32_MAX;
+	uint DirectionalLightSize = UINT32_MAX;
+	uint PointLightOffset = UINT32_MAX;
+	uint PointLightCount = UINT32_MAX;
+	uint PointLightSize = UINT32_MAX;
+};
+
+struct Mesh;
+struct GPUMaterial;
+struct DirectionalLight;
+struct PointLight;
+class MemoryPoolSystem
+{
+public:
+	static MemoryPoolSystem& Get();
+
+private:
+	MemoryPoolSystem() = default;
+	~MemoryPoolSystem() = default;
+	MemoryPoolSystem(const MemoryPoolSystem&) = delete;
+	MemoryPoolSystem& operator=(const MemoryPoolSystem&) = delete;
+	MemoryPoolSystem(MemoryPoolSystem&&) = delete;
+	MemoryPoolSystem& operator=(MemoryPoolSystem&&) = delete;
+
+	static constexpr size_t MeshInitialCapacity = 4096;
+	static constexpr size_t MaterialInitialCapacity = 65536;
+	static constexpr size_t DirectionalLightInitialCapacity = 4;
+	static constexpr size_t PointLightInitialCapacity = 4096;
+
+	UnorderedMap<MemoryPoolTypes, MemoryPoolSubBufferHeader> MemorySubPoolHeader;
+
+	void   ResizeMemoryPool(MemoryPoolTypes memoryPoolToUpdate);
+	void   UpdateMemoryPoolHeader();
+
+public:
+	uint32					GPUBufferIndex = UINT32_MAX;
+	size_t					GPUBufferMemoryPoolSize = UINT32_MAX;
+	MemoryPoolBufferHeader  GPUMemoryPoolHeader;
+	Vector<byte>			GPUBufferMemoryPool;
+	bool					IsHeaderDirty = true;
+	bool					IsBufferDirty = true;
+
+	void   StartUp();
+	uint32 AllocateObject();
+	const Vector<VkDescriptorBufferInfo> GetMemoryPoolBufferInfo() const;
+};
+extern DLL_EXPORT MemoryPoolSystem& memoryPoolSystem;
+inline MemoryPoolSystem& MemoryPoolSystem::Get()
+{
+	static MemoryPoolSystem instance;
+	return instance;
+}
