a2d47690e51f08e72f9888da62258fe75472b4d0
ThomasDHZ <dothackzero@outlook.com>
2026-09-07 18:07:01 -0400

    Updating more level editor stuff.

---
+++ GameScriptLibraryDLL/Components/Collider2DComponent.cs
@@ -0,0 +1,20 @@
+﻿using GlmSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameScriptLibraryDLL.Components
+{
+    public struct Collider2DComponent
+    {
+        public ivec2 Size { get; set; } = new ivec2(32, 32);
+        public ivec2 Offset { get; set; } = new ivec2(0, 0);
+        public bool Enabled { get; set; } = true;
+        public bool IsTrigger { get; set; } = false;
+        public Collider2DComponent()
+        {
+        }
+    }
+}
