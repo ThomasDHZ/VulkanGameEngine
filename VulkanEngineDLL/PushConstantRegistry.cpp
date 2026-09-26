7908295ecc992a4d2ccec4d2f5ba7cd76761dcc4
ThomasDHZ <dothackzero@outlook.com>
2026-09-26 10:17:44 -0400

    a

--- VulkanEngineDLL/PushConstantRegistry.cpp
+++ VulkanEngineDLL/PushConstantRegistry.cpp
@@ -8,7 +8,7 @@ PushConstantRegistry& pushConstantRegistry = PushConstantRegistry::Get();
 
 void PushConstantRegistry::RegisterPushConstantValue(const String& sourceName, UpdateFunc func)
 {
-	registry[sourceName] = std::move(func);
+    registry[sourceName] = std::move(func);
 }
 
 void PushConstantRegistry::ApplyPushConstantRules(ShaderPushConstant& pushConstant, const PushConstantContext& pushConstantContext)
@@ -21,23 +21,23 @@ void PushConstantRegistry::ApplyPushConstantRules(ShaderPushConstant& pushConsta
     }
     for (auto& pushConstantVariable : pushConstantContext.PushConstantUpdateRules)
     {
-        if(shaderSystem.SearchPushConstantForVariableExists(pushConstant.PushConstantName, pushConstantVariable.Variable));
+        if (shaderSystem.SearchPushConstantForVariableExists(pushConstant.PushConstantName, pushConstantVariable.Variable))
         {
             switch (pushConstantVariable.VariableType)
             {
-                case kShaderMember_Int:   shaderSystem.UpdatePushConstantValue<int>(pushConstant,   pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<int>(pushConstantVariable));   break;
-                case kShaderMember_Uint:  shaderSystem.UpdatePushConstantValue<uint>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<uint>(pushConstantVariable));  break;
-                case kShaderMember_Float: shaderSystem.UpdatePushConstantValue<float>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<float>(pushConstantVariable)); break;
-                case kShaderMember_Ivec2: shaderSystem.UpdatePushConstantValue<ivec2>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<ivec2>(pushConstantVariable)); break;
-                case kShaderMember_Ivec3: shaderSystem.UpdatePushConstantValue<ivec3>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<ivec3>(pushConstantVariable)); break;
-                case kShaderMember_Ivec4: shaderSystem.UpdatePushConstantValue<ivec4>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<ivec4>(pushConstantVariable)); break;
-                case kShaderMember_Vec2:  shaderSystem.UpdatePushConstantValue<vec2>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<vec2>(pushConstantVariable));  break;
-                case kShaderMember_Vec3:  shaderSystem.UpdatePushConstantValue<vec3>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<vec3>(pushConstantVariable));  break;
-                case kShaderMember_Vec4:  shaderSystem.UpdatePushConstantValue<vec4>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<vec4>(pushConstantVariable));  break;
-                case kShaderMember_Mat2:  shaderSystem.UpdatePushConstantValue<mat2>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<mat2>(pushConstantVariable));  break;
-                case kShaderMember_Mat3:  shaderSystem.UpdatePushConstantValue<mat3>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<mat3>(pushConstantVariable));  break;
-                case kShaderMember_Mat4:  shaderSystem.UpdatePushConstantValue<mat4>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<mat4>(pushConstantVariable));  break;
-                case kShaderMember_bool:  shaderSystem.UpdatePushConstantValue<bool>(pushConstant,  pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<bool>(pushConstantVariable));  break;
+            case kShaderMember_Int:   shaderSystem.UpdatePushConstantValue<int>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<int>(pushConstantVariable));   break;
+            case kShaderMember_Uint:  shaderSystem.UpdatePushConstantValue<uint>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<uint>(pushConstantVariable));  break;
+            case kShaderMember_Float: shaderSystem.UpdatePushConstantValue<float>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<float>(pushConstantVariable)); break;
+            case kShaderMember_Ivec2: shaderSystem.UpdatePushConstantValue<ivec2>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<ivec2>(pushConstantVariable)); break;
+            case kShaderMember_Ivec3: shaderSystem.UpdatePushConstantValue<ivec3>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<ivec3>(pushConstantVariable)); break;
+            case kShaderMember_Ivec4: shaderSystem.UpdatePushConstantValue<ivec4>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<ivec4>(pushConstantVariable)); break;
+            case kShaderMember_Vec2:  shaderSystem.UpdatePushConstantValue<vec2>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<vec2>(pushConstantVariable));  break;
+            case kShaderMember_Vec3:  shaderSystem.UpdatePushConstantValue<vec3>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<vec3>(pushConstantVariable));  break;
+            case kShaderMember_Vec4:  shaderSystem.UpdatePushConstantValue<vec4>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<vec4>(pushConstantVariable));  break;
+            case kShaderMember_Mat2:  shaderSystem.UpdatePushConstantValue<mat2>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<mat2>(pushConstantVariable));  break;
+            case kShaderMember_Mat3:  shaderSystem.UpdatePushConstantValue<mat3>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<mat3>(pushConstantVariable));  break;
+            case kShaderMember_Mat4:  shaderSystem.UpdatePushConstantValue<mat4>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<mat4>(pushConstantVariable));  break;
+            case kShaderMember_bool:  shaderSystem.UpdatePushConstantValue<bool>(pushConstant, pushConstantVariable.Variable, shaderSystem.GetPushConstantValue<bool>(pushConstantVariable));  break;
             }
         }
     }
@@ -69,6 +69,12 @@ void PushConstantRegistry::RegisterDefaultPushConstantRules()
 
             shaderSystem.UpdatePushConstantValue<uint>(pushConstant, "CubeMapResolution", context.RenderPassResolution.x);
             shaderSystem.UpdatePushConstantValue<float>(pushConstant, "Roughness", roughness);
+            shaderSystem.UpdatePushConstantBuffer(pushConstant);
+        });
+
+    pushConstantRegistry.RegisterPushConstantValue("materialBaker", [&](ShaderPushConstant& pushConstant, const PushConstantContext& context)
+        {
+            shaderSystem.UpdatePushConstantValue<uint>(pushConstant, "MaterialBakerSubPassIndex", context.DrawIndex);
             shaderSystem.UpdatePushConstantBuffer(pushConstant);
         });
 }
